d43778dcbf6b67fc89df001400295a1e34a4f4ca
ijauregiCMCRC <inigo.jauregi@rozettainstitute.com>
2021-10-17 16:31:36 +1100

    Translation notebook, main functions. Move requirements.txt inside translation folder.

--- translation/src/translation_lightning_model.py
+++ translation/src/translation_lightning_model.py
@@ -1,27 +1,20 @@
-# Script to train the language model for text classification
-
-# Script to train the joint model
-import os
-import argparse
-import random
-import numpy as np
+# Script to train a translation model with Pytorch Lightning and
+# a pretrainedSeq2seq model from Huggingface
 
 import torch
-from torch.utils.data import DataLoader, Dataset
+from torch.utils.data import DataLoader
 from torch.nn.parallel import DistributedDataParallel
-import transformers
-import transformers.adapters.composition as ac
+
 from transformers import AutoModelForSeq2SeqLM, AutoTokenizer, MBartTokenizer
 from transformers.adapters.configuration import AdapterConfig
-from transformers.optimization import get_linear_schedule_with_warmup, Adafactor
+from transformers.optimization import get_linear_schedule_with_warmup
+
 from sacrebleu.metrics import BLEU
 
 import pytorch_lightning as pl
-from pytorch_lightning.loggers import TestTubeLogger
-from pytorch_lightning.callbacks import ModelCheckpoint
-# from pytorch_lightning.overrides.data_parallel import LightningDistributedDataParallel
 
 from src.translation_metrics import translation_accuracy
+from src.translation_dataset import TranslationDataset
 
 
 class LmForTranslation(pl.LightningModule):
@@ -30,36 +23,28 @@ class LmForTranslation(pl.LightningModule):
         super().__init__()
         self.args = params
         self.hparams['params'] = params
-        self.tokenizer = AutoTokenizer.from_pretrained(self.args.tokenizer, use_fast=False)
+        self.tokenizer = AutoTokenizer.from_pretrained(self.args['tokenizer'], use_fast=False)
         if isinstance(self.tokenizer, MBartTokenizer):
-            # print(self.tokenizer._src_lang)
-            # print(self.tokenizer.tgt_lang)
-            src_lan = self.args.src + "_XX"
-            tgt_lan = self.args.tgt + "_XX"  # + self.args.tgt.upper()
+            src_lan = self.args['src'] + "_XX"
+            tgt_lan = self.args['tgt'] + "_XX"
             self.tokenizer._src_lang = src_lan
             self.tokenizer.tgt_lang = tgt_lan
-        self.tokenizer.save_pretrained(self.args.save_dir, self.args.save_prefix)
-        self.model = AutoModelForSeq2SeqLM.from_pretrained(self.args.model_lm_path)
 
-        if self.args.add_adapter:
-            config = AdapterConfig.load("pfeiffer", non_linearity="relu", reduction_factor=1)
+        self.model = AutoModelForSeq2SeqLM.from_pretrained(self.args['model'])
+
+        if self.args['add_adapter']:
+            config = AdapterConfig.load("pfeiffer", non_linearity="relu",
+                                        reduction_factor=self.args['reduction_factor'])
             self.model.add_adapter("mt_adapter", config=config)
             self.model.train_adapter("mt_adapter")
 
         self.train_dataloader_object = self.val_dataloader_object = self.test_dataloader_object = None
 
-        # self.writer = open('lightning_preds.txt', 'w')
-        # self.writer_label = open('lightning_labels.txt', 'w')
-
     def forward(self, src_ids, src_attention_mask, tgt_ids, tgt_attention_mask):
 
-        # for name, p in self.model.named_parameters():
-        #     print('Name: ', name, '  (required_grad=', p.requires_grad, ')')
-
         labels = tgt_ids[:, 1:].clone()
-        # print([self.tokenizer.decode(t, skip_special_tokens=False) for t in labels])
         decoder_input_ids = tgt_ids[:, :-1]
-        # print([self.tokenizer.decode(t, skip_special_tokens=False) for t in decoder_input_ids])
+
         decoder_attention_mask = (decoder_input_ids != self.tokenizer.pad_token_id)
         outputs = self.model(
             src_ids,
@@ -69,21 +54,42 @@ class LmForTranslation(pl.LightningModule):
             use_cache=False
         )
         lm_logits = outputs.logits
-        if self.args.label_smoothing == 0:
-            # Same behavior as modeling_bart.py, besides ignoring pad_token_id
+        if self.args['label_smoothing'] == 0:
             ce_loss_fct = torch.nn.CrossEntropyLoss(ignore_index=self.tokenizer.pad_token_id)
             assert lm_logits.shape[-1] == self.model.config.vocab_size  # It has to be the same as the output class
             loss = ce_loss_fct(lm_logits.view(-1, lm_logits.shape[-1]), labels.view(-1))
         else:
             lprobs = torch.nn.functional.log_softmax(lm_logits, dim=-1)
             loss, nll_loss = self.label_smoothed_nll_loss(
-                lprobs, labels, self.args.label_smoothing, ignore_index=self.tokenizer.pad_token_id
+                lprobs, labels, self.args['label_smoothing'], ignore_index=self.tokenizer.pad_token_id
             )
 
         # Metrics
         acc = translation_accuracy(lm_logits.detach(), labels, tokenizer=self.tokenizer)
 
         return [loss, acc]
+
+    def translate_example(self, sentence):
+
+        sen_ids = self.tokenizer.encode(sentence.strip())
+        sen_attention_mask = [1] * len(sen_ids)
+
+        sen_ids = torch.nn.utils.rnn.pad_sequence([torch.tensor(sen_ids)], batch_first=True, padding_value=1)
+        sen_attention_mask = torch.nn.utils.rnn.pad_sequence([torch.tensor(sen_attention_mask)],
+                                                             batch_first=True, padding_value=0)
+
+        if isinstance(self.tokenizer, MBartTokenizer):
+            decoder_start_token_id = self.tokenizer.lang_code_to_id["en_XX"]
+        else:
+            decoder_start_token_id = self.tokenizer.bos_token_id
+        generated_ids = self.model.generate(input_ids=sen_ids, attention_mask=sen_attention_mask,
+                                            decoder_start_token_id=decoder_start_token_id,
+                                            max_length=self.args['max_tgt_len'],
+                                            num_beams=1, num_beam_groups=1, do_sample=False)
+
+        generated_str = self.tokenizer.batch_decode(generated_ids.tolist(), skip_special_tokens=True)
+
+        return generated_str
 
     @staticmethod
     def label_smoothed_nll_loss(lprobs, target, epsilon, ignore_index=-100):
@@ -130,10 +136,13 @@ class LmForTranslation(pl.LightningModule):
         vloss = outputs[0]
         # Generate translation
         src_ids, src_attention_mask, tgt_ids, tgt_attention_mask = batch
-        src_str = self.tokenizer.batch_decode(src_ids.tolist(), skip_special_tokens=True)
+        if isinstance(self.tokenizer, MBartTokenizer):
+            decoder_start_token_id = self.tokenizer.lang_code_to_id["en_XX"]
+        else:
+            decoder_start_token_id = self.tokenizer.bos_token_id
         generated_ids = self.model.generate(input_ids=src_ids, attention_mask=src_attention_mask,
-                                            decoder_start_token_id=self.tokenizer.lang_code_to_id["en_XX"],
-                                            max_length=self.args.max_output_len,
+                                            decoder_start_token_id=decoder_start_token_id,
+                                            max_length=self.args['max_tgt_len'],
                                             num_beams=1, num_beam_groups=1, do_sample=False)
         generated_str = self.tokenizer.batch_decode(generated_ids.tolist(), skip_special_tokens=True)
         gold_str = self.tokenizer.batch_decode(tgt_ids.tolist(), skip_special_tokens=True)
@@ -141,7 +150,7 @@ class LmForTranslation(pl.LightningModule):
         return {'vloss': vloss, 'vaccuracy': outputs[1], 'ref_sentences': gold_str, 'pred_sentences': generated_str}
 
     def validation_epoch_end(self, outputs, test=False):
-        if self.args.add_adapter:
+        if self.args['add_adapter']:
             for name, p in self.model.named_parameters():
                 if 'adapters' in name:
                     p.requires_grad = True
@@ -164,27 +173,14 @@ class LmForTranslation(pl.LightningModule):
         for x in outputs:
             for ref in x['ref_sentences']:
                 ref_sentences[0].append(ref)
-                # print(x['ref_sentences'])
-                # ref_sentences += x['ref_sentences']
             for pred in x['pred_sentences']:
                 pred_sentences.append(pred)
-                # print(x['pred_sentences'])
-                # pred_sentences += x['pred_sentences']
         bleu_scorer = BLEU()
         bleu_score = bleu_scorer.corpus_score(pred_sentences, ref_sentences)
         logs = dict(zip(*[names, metrics]))
         self.log("BLEU", bleu_score.score, prog_bar=True)
-        # print(logs)
-
-        # Save language model
-        if test is not True:
-            self.save_language_model(bleu_score.score)
 
         return {'avg_val_loss': logs['vloss'], 'avg_accuracy': logs['vaccuracy'], 'log': logs, 'progress_bar': logs}
-
-    def save_language_model(self, bleu_score):
-        path_save = os.path.join(self.args.save_dir, self.args.save_prefix, "checkpoints_" + str(bleu_score))
-        self.model.save_pretrained(path_save, True)
 
     def test_step(self, batch, batch_nb):
         return self.validation_step(batch, batch_nb)
@@ -194,25 +190,26 @@ class LmForTranslation(pl.LightningModule):
         print(result)
 
     def configure_optimizers(self):
-        if self.args.adafactor:
-            optimizer = Adafactor(self.model.parameters(), lr=self.args.lr, scale_parameter=False, relative_step=False)
-        else:
-            optimizer = torch.optim.AdamW(self.model.parameters(), lr=self.args.lr, weight_decay=self.args.weight_decay)
-        if self.args.debug:
-            return optimizer  # const LR
+        # Define AdamW optimizer
+        optimizer = torch.optim.AdamW(self.model.parameters(), lr=self.args['lr'],
+                                      weight_decay=self.args['weight_decay'])
+
         num_gpus = torch.cuda.device_count() if torch.cuda.is_available() else 1
-        num_steps = self.args.dataset_size * self.args.epochs / num_gpus / self.args.grad_accum / self.args.batch_size
+        print(self.args)
+        num_steps = self.args['dataset_size'] * \
+                    self.args['epochs'] / num_gpus / self.args['grad_accum'] / self.args['batch_size']
         scheduler = get_linear_schedule_with_warmup(
-            optimizer, num_warmup_steps=self.args.warmup, num_training_steps=num_steps
+            optimizer, num_warmup_steps=self.args['warmup'], num_training_steps=num_steps
         )
         return [optimizer], [{"scheduler": scheduler, "interval": "step"}]
 
     def _get_dataloader(self, current_dataloader, split_name, is_train):
         if current_dataloader is not None:
             return current_dataloader
-        dataset = TranslationDataset(hf_dataset=self.hf_datasets[split_name], src_prefix=self.args.src,
-                                     tgt_prefix=self.args.tgt, tokenizer=self.tokenizer,
-                                     max_input_len=self.args.max_input_len, max_output_len=self.args.max_output_len)
+        dataset = TranslationDataset(hf_dataset=self.hf_datasets[split_name], src_prefix=self.args['src'],
+                                     tgt_prefix=self.args['tgt'], tokenizer=self.tokenizer,
+                                     max_input_len=self.args['max_src_len'],
+                                     max_output_len=self.args['max_tgt_len'])
         sampler = torch.utils.data.distributed.DistributedSampler(dataset,
                                                                   shuffle=is_train) if \
             self.trainer.accelerator_connector.use_ddp else None
@@ -221,9 +218,8 @@ class LmForTranslation(pl.LightningModule):
             is_shuffle = True
         else:
             is_shuffle = False
-        return DataLoader(dataset, batch_size=self.args.batch_size, shuffle=is_shuffle,
-                          num_workers=self.args.num_workers, sampler=sampler,
-                          collate_fn=TranslationDataset.collate_fn)
+        return DataLoader(dataset, batch_size=self.args['batch_size'], shuffle=is_shuffle,
+                          num_workers=1, sampler=sampler, collate_fn=TranslationDataset.collate_fn)
 
     def train_dataloader(self):
         self.train_dataloader_object = self._get_dataloader(self.train_dataloader_object, 'train', is_train=True)
@@ -244,54 +240,3 @@ class LmForTranslation(pl.LightningModule):
             find_unused_parameters=False
         )
         return model
-
-    @staticmethod
-    def add_model_specific_args(parser, root_dir):
-        parser.add_argument("--train_data", type=str, required=True, help='Path to training data')
-        parser.add_argument("--validation_data", type=str, required=True, help='Path to validation data')
-        parser.add_argument("--test_data", type=str, required=True, help='Path to testing data')
-        parser.add_argument("--src", type=str, required=True, help='Source language.')
-        parser.add_argument("--tgt", type=str, required=True, help='Target language.')
-        parser.add_argument("--save_dir", type=str, default='translation')
-        parser.add_argument("--save_prefix", type=str, default='test')
-        parser.add_argument("--batch_size", type=int, default=16, help="Batch size")
-        parser.add_argument("--grad_accum", type=int, default=1, help="number of gradient accumulation steps")
-        parser.add_argument("--max_grad_norm", type=float, default=1.0, help="number of gradient accumulation steps")
-        parser.add_argument("--gpus", type=int, default=0,
-                            help="Number of gpus. 0 for CPU")
-        parser.add_argument("--warmup", type=int, default=500, help="Number of warmup steps")
-        parser.add_argument("--lr", type=float, default=0.00003, help="Maximum learning rate")
-        parser.add_argument("--weight_decay", type=float, default=0.01, help="Adam weight decay")
-        parser.add_argument("--val_every", type=float, default=1.0, help="Number of training steps between validations")
-        parser.add_argument("--val_percent_check", default=1.00, type=float, help='Percent of validation data used')
-        parser.add_argument("--num_workers", type=int, default=0, help="Number of data loader workers")
-        parser.add_argument("--seed", type=int, default=1234, help="Seed")
-        parser.add_argument("--epochs", type=int, default=1, help="Number of epochs")
-        parser.add_argument("--disable_checkpointing", action='store_true', help="No logging or checkpointing")
-        parser.add_argument("--max_input_len", type=int, default=170,
-                            help="maximum num of wordpieces/summary. Used for training and testing")
-        parser.add_argument("--max_output_len", type=int, default=170,
-                            help="maximum num of wordpieces/summary. Used for training and testing")
-        parser.add_argument("--test", action='store_true', help="Test only, no training")
-        parser.add_argument("--model_lm_path", type=str, default='../pretrained_lms/sshleifer-tiny-mbart',
-                            help="Path to the checkpoint directory or model name")
-        parser.add_argument("--tokenizer", type=str, default='../pretrained_lms/sshleifer-tiny-mbart')
-        parser.add_argument("--add_adapter", action='store_true', help="Add an adapter.")
-        parser.add_argument("--progress_bar", type=int, default=10, help="Progress bar. Good for printing")
-        parser.add_argument("--precision", type=int, default=32, help="Double precision (64), full precision (32) "
-                                                                      "or half precision (16). Can be used on CPU, "
-                                                                      "GPU or TPUs.")
-        parser.add_argument("--amp_backend", type=str, default='native', help="The mixed precision backend to "
-                                                                              "use ('native' or 'apex')")
-        parser.add_argument("--debug", action='store_true', help="debug run")
-        parser.add_argument("--resume_ckpt", type=str, help="Path of a checkpoint to resume from")
-        parser.add_argument("--from_pretrained", type=str, default=None,
-                            help="Path to a checkpoint to load model weights but not training state")
-        parser.add_argument('--grad_ckpt', action='store_true', help='Enable gradient checkpointing to save memory')
-        parser.add_argument("--attention_dropout", type=float, default=0.1, help="attention dropout")
-        parser.add_argument("--attention_mode", type=str, default='sliding_chunks', help="Longformer attention mode")
-        parser.add_argument("--attention_window", type=int, default=512, help="Attention window")
-        parser.add_argument("--label_smoothing", type=float, default=0.0, required=False)
-        parser.add_argument("--adafactor", action='store_true', help="Use adafactor optimizer")
-
-        return parser
